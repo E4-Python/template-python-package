2ad40b1d54b0eff29f02ab25180020375d31ec00
이정훈 <eu4ng97@gmail.com>
2025-05-26 05:35:00 +0900

    Feat: 패키지 템플릿 추가

---
+++ PACKAGE/MODULE.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def print_array():
+    array = np.arange(10)
+    print(array)
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='PACKAGE',
+    version='0.1.0',
+    description='DESCRIPTION',
+    url='https://github.com/E4-Python/template-python-package',
+    author='Eu4ng',
+    author_email='eu4ng97@gmail.com',
+    packages=['PACKAGE'],
+    install_requires=[
+        'numpy>=2.2.6'
+    ]
+)
